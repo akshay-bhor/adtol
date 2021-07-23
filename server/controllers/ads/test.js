9cc2abac2feaa8059b7bf6542b6001bb515d8147
Akshay Bhor <akshay.bhor@ignitesol.com>
2021-07-23 17:38:21 +0530

    github actions fix 6

---
+++ server/controllers/ads/test.js
@@ -0,0 +1,30 @@
+const Campaigns = require('../../models/campaigns');
+
+exports.test = async(req, res, next) => {
+    // const Countries = require('../../models/countries');
+
+    // let tmp = [];
+    // let countries = Countries.findAll({ order:[['id', 'ASC']], attributes: ['id', 'code', 'name'] })
+    //     .then(res => { console.log(res);
+    //         res.forEach(data => {
+    //             tmp[data.dataValues.id] = [data.dataValues.code, data.dataValues.name];
+    //         });
+    //         let cData = {...tmp};
+    //         let cn = 'IN';
+    //         console.log(Object.keys(cData).find(key => cData[key][0] == cn));
+    //     });
+        
+
+    const parser = require('ua-parser-js');
+
+    let uastr = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36";
+
+    let ua = parser(uastr);
+
+    console.log(ua.device);
+
+    const r = await Campaigns.findOne({ where: { id: 5 }, attributes: ['uid', 'url', 'cpc'] });
+
+    console.log(r);
+    res.redirect('http://adtol.com');
+}
