c6077505dab84113cf4fa720078f76b854c7245b
Akshay Bhor <akshay.bhor@ignitesol.com>
2021-10-08 11:00:28 +0530

    query logging true

--- server/utils/db.js
+++ server/utils/db.js
@@ -4,7 +4,7 @@ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, proces
     host: process.env.DB_HOST,
     dialect: 'mysql',
     port: process.env.DB_PORT,
-    logging: false,
+    logging: true,
     pool: {
         max: 10,
         min: 0,
