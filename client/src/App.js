1f7df51acfdbc3d60a15dfdcbb9e42dc6ec9129c
Akshay Bhor <akshay.bhor@ignitesol.com>
2021-10-09 23:56:51 +0530

    Added page view tracking

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Suspense, lazy, useEffect } from "react";
 import { useSelector } from "react-redux";
 import { Redirect, Route, Switch } from "react-router";
+import { useLocation } from "react-router-dom";
 import TagManager from "react-gtm-module";
 import "./App.css";
 import Loading from "./Components/UI/Loading";
@@ -29,7 +30,7 @@ const ContactPage = lazy(() => import("./Pages/Contact/Contact"));
 
 function App() {
   const isAuth = useSelector((state) => state.auth.loggedIn);
-
+  const location = useLocation();
   /**
    * Google Tag manager for analytics
    */
@@ -39,7 +40,16 @@ function App() {
     }
    
     TagManager.initialize(tagManagerArgs);
-  }, [])
+  }, []);
+
+  // Track pageViews
+  useEffect(() => {
+    if(window.dataLayer) {
+      window.dataLayer.push({
+        event: 'pageview'
+      });
+    }
+  }, [location]);
 
   return (
     <Layout>
