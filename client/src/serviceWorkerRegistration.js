11fbf5d9c50da173ee1c66954035247a9b93d9c9
Akshay Bhor <akbhor50@gmail.com>
2021-10-15 13:17:25 +0530

    Revert "sw update"
    This reverts commit bc733a785927f0f29a0b0445ef078a4dbfc4cf61.

--- client/src/serviceWorkerRegistration.js
+++ client/src/serviceWorkerRegistration.js
@@ -72,16 +72,17 @@ function registerValidSW(swUrl, config) {
                   'tabs for this page are closed. See https://cra.link/PWA.'
               );
 
-              // Skip waiting for new sw to activate
-              registration.waiting.postMessage({ type: 'SKIP_WAITING' });
-
-              // Force cold boot
-              window.location.reload();
-
               // Execute callback
               if (config && config.onUpdate) {
                 config.onUpdate(registration);
               }
+
+              // Skip waiting for new sw to activate
+              registration.waiting.postMessage({ type: 'SKIP_WAITING' });
+
+              // Force cold boot
+              const loc = window.location;
+              window.location.href = loc + '?force_cold_boot=1';
             } else {
               // At this point, everything has been precached.
               // It's the perfect time to display a
