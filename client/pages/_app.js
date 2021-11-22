ab019df9729cfbbf911fa8c5d2d10dd752ab131f
Akshay Bhor <akbhor50@gmail.com>
2021-11-22 23:46:08 +0530

    bugfix server side injected css 2

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -50,15 +50,12 @@ export default function MyApp({ Component, pageProps }) {
         event: 'pageview'
       });
     }
-  }, [router.pathname]);
-
-  useEffect(() => {
     // Remove the server-side injected CSS.
     const jssStyles = document.querySelector('#jss-server-side');
     if (jssStyles) {
       jssStyles.parentElement.removeChild(jssStyles);
     }
-  }, []);
+  }, [router.pathname]);
 
   return (
     <>
