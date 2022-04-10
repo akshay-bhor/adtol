22aa7bac95068451cdedcc7ea727af4ff3b3c583
Akshay Bhor <akbhor50@gmail.com>
2022-04-10 22:30:41 +0530

    deadlock fix

--- server/controllers/ads/ad-serve.js
+++ server/controllers/ads/ad-serve.js
@@ -301,10 +301,17 @@ exports.adServe = async (req, res, next) => {
  */
 const executeUpdateQuery = async (table, day_unix, col, value, campaign, website, ad_uid, pub_uid) => {
     const incCount = 1;
-    return sequelize.query(`UPDATE ${table} SET views = views + ${incCount} WHERE day_unix = ? AND ${col} = ? AND (campaign = ? OR website = ?) AND (ad_uid = ? OR pub_uid = ?)`, {
-        type: QueryTypes.UPDATE,
-        replacements: [day_unix, value, campaign, website, ad_uid, pub_uid]
-    });
+    
+    const update1 = await sequelize.query(`UPDATE ${table} SET views = views + ${incCount} WHERE day_unix = ? AND ${col} = ? AND campaign = ? AND ad_uid = ?`, {
+            type: QueryTypes.UPDATE,
+            replacements: [day_unix, value, campaign, ad_uid]
+        });
+    const update2 = await sequelize.query(`UPDATE ${table} SET views = views + ${incCount} WHERE day_unix = ? AND ${col} = ? AND website = ? AND pub_uid = ?`, {
+            type: QueryTypes.UPDATE,
+            replacements: [day_unix, value, website, pub_uid]
+        });
+
+    return [null, Number(update1[1] + update2[1])];
 }
 
 /**
